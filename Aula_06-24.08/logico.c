8f5740079dd9325f55e090703168a79635be60d9
Sara Cristina <94648070+SarinhaX4@users.noreply.github.com>
2023-08-24 11:30:27 -0300

    24.08

---
+++ Aula_06-24.08/logico.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// main(){
+//   //verificar a aprovação do aluno 
+//   // media >= 7.0
+//   // frequencia >= 75
+
+//   int media = 6;
+//   int frequencia = 80;
+
+//   if(media >= 7 && frequencia >= 75){
+//     printf("aprovado");
+//   } else{
+//     printf("reprovado");
+//   }
+  
+  
+// }
---
+++ Aula_06-24.08/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// main() {
+//   int idade;
+//   char nome[40];
+
+//   printf("Digite seu nome:");
+//   fgets(nome, 40, stdin);
+
+//   printf("Digite sua idade:");
+//   scanf("%d", &idade);
+
+//   if(idade < 18){
+//     printf("%s", nome);
+//     printf("é menor de idade");
+//   }
+//   else{
+//     printf("%s", nome);
+//     printf("é maior de idade");
+//   }
+// }
